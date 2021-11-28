e39579a502081e1c9ef474d41618a0be45b214ff
FlickerSoul <13360148+FlickerSoul@users.noreply.github.com>
2021-11-27 22:28:16 -0800

    temp: remove IMPORT_WHITE_LIST and add EXEC_MEM_OUT

--- executor/settings/variables.py
+++ executor/settings/variables.py
@@ -37,33 +37,46 @@ class DefaultVars(VarClass):
     SERVER_PORT = "SERVE_PORT"
     ALLOW_ORIGIN = "ALLOWED_ORIGIN"
     EXEC_TIME_OUT = "EXEC_TIME_OUT"
+    EXEC_MEM_OUT = "EXEC_MEM_OUT"
     LOG_CMD_OUTPUT = "LOG_CMD_OUTPUT"
     IS_DEV = "IS_DEV"
     DEFAULT_RAND_SEED = "DEFAULT_RAND_SEED"
     DEFAULT_FLOAT_PRECISION = "DEFAULT_FLOAT_PRECISION"
-    IMPORT_WHITE_LIST = "IMPORT_WHITE_LIST"
     ALLOW_BUILTIN_FUNCTIONS = "ALLOW_BUILTIN_FUNCTIONS"
 
     vars = {
         SERVER_URL: "127.0.0.1",
         SERVER_PORT: "7590",
-        ALLOW_ORIGIN: None,
+        ALLOW_ORIGIN: False,
         EXEC_TIME_OUT: 5,
+        EXEC_MEM_OUT: 100,
         LOG_CMD_OUTPUT: True,
         IS_DEV: False,
         DEFAULT_RAND_SEED: 0,
         DEFAULT_FLOAT_PRECISION: 4,
-        IMPORT_WHITE_LIST: [],
         ALLOW_BUILTIN_FUNCTIONS: False,
     }
 
     @classmethod
-    def read_from_env(cls, *args, use_default: bool = False) -> None:
+    def read_from_env(
+        cls, *args, use_default: bool = False, all_arg: bool = False
+    ) -> None:
+        if all_arg:
+            args = cls.vars.keys()
+
         for env_name in args:
+            original = cls.vars[env_name]
+            # type conversions from str to the proper type
+            og_type = type(original)
+
             if use_default:
-                setattr(cls, env_name, getenv(env_name, cls.var[env_name]))
+                cls.vars[env_name] = og_type(getenv(env_name, original))
             else:
-                setattr(cls, env_name, getenv(env_name))
+                cls.vars[env_name] = og_type(getenv(env_name))
+
+    @classmethod
+    def __class_getitem__(cls, item):
+        return cls.vars[item]
 
 
 # Custom Variables
