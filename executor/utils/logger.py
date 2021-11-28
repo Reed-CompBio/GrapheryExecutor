ac06ccb955816821e89014f450ef63fdc0980785
FlickerSoul <13360148+FlickerSoul@users.noreply.github.com>
2021-11-28 02:31:25 -0800

    feat: sketched controller

---
+++ executor/utils/logger.py
@@ -0,0 +1,20 @@
+import logging
+
+
+def _init_void_logger():
+    _logger = logging.getLogger("void")
+    _logger.addHandler(logging.NullHandler())
+    return _logger
+
+
+void_logger = _init_void_logger()
+
+
+def _init_shell_debug_logger():
+    _logger = logging.getLogger("executor.shell_debug")
+    _logger.setLevel(logging.DEBUG)
+    _logger.addHandler(logging.StreamHandler())
+    return _logger
+
+
+shell_debug_logger = _init_shell_debug_logger()
