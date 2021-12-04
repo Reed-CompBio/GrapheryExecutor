1840ec3528619169b70495488e7c52236484b916
FlickerSoul <13360148+FlickerSoul@users.noreply.github.com>
2021-12-03 21:18:22 -0800

    feat: move argparse and use controller in execute

--- executor/server_utils/utils.py
+++ executor/server_utils/utils.py
@@ -1,10 +1,8 @@
 from __future__ import annotations
 
-import argparse
-from typing import Mapping, Any, Union, Tuple, Iterable, List
+from typing import Mapping, Any, Tuple, Iterable, List
 
-from networkx import Graph
-from ..settings import VarClass, DefaultVars
+from executor.utils.controller import GraphController
 
 
 class ExecutionServerException(Exception):
@@ -21,30 +19,6 @@ class ExecutionException(Exception):
         return len(self.related_exec_info) == 0
 
 
-def arg_parser(settings: VarClass = DefaultVars) -> Mapping[str, Union[int, str]]:
-    parser = argparse.ArgumentParser(
-        prog="graphery_executor", description="Graphery Executor Server"
-    )
-    exec_parser_group = parser.add_subparsers(required=True)
-
-    # server parser
-    server_parser = exec_parser_group.add_parser("server")
-    for arg, kwargs in settings.server_shell_var.values():
-        server_parser.add_argument(*arg, **kwargs)
-
-    # local parser
-    local_parser = exec_parser_group.add_parser("local")
-    local_parser.add_argument("code")
-    local_parser.add_argument("graph")
-
-    # options for all
-    for arg, kwargs in settings.general_shell_var.values():
-        parser.add_argument(*arg, **kwargs)
-
-    args: argparse.Namespace = parser.parse_args()
-    return vars(args)
-
-
 def create_error_response(message: str) -> dict:
     return {"errors": [{"message": message}]}
 
@@ -53,7 +27,7 @@ def create_data_response(data: Any) -> dict:
     return {"data": data if isinstance(data, Mapping) else {"info": data}}
 
 
-def execute(
-    code: str, graph_json: Union[str, Mapping], auto_delete_cache: bool = False
-) -> Tuple[str, List[Mapping]]:
-    pass
+def execute(code: str, graph_json: str, options: Mapping = None) -> List[Mapping]:
+    options = options or {}
+    ctrl = GraphController(code=code, graph_data=graph_json, options=options).init()
+    return ctrl.main()
--- executor/utils/cli.py
+++ executor/utils/cli.py
@@ -1,4 +1,33 @@
-from ..server_utils.utils import arg_parser
+from __future__ import annotations
+
+import argparse
+from typing import Mapping, Union
+
+from executor.settings import DefaultVars
+
+
+def arg_parser(settings: DefaultVars = DefaultVars) -> Mapping[str, Union[int, str]]:
+    parser = argparse.ArgumentParser(
+        prog="graphery_executor", description="Graphery Executor Server"
+    )
+    exec_parser_group = parser.add_subparsers(required=True)
+
+    # server parser
+    server_parser = exec_parser_group.add_parser("server")
+    for arg, kwargs in settings.server_shell_var.values():
+        server_parser.add_argument(*arg, **kwargs)
+
+    # local parser
+    local_parser = exec_parser_group.add_parser("local")
+    local_parser.add_argument("code")
+    local_parser.add_argument("graph")
+
+    # options for all
+    for arg, kwargs in settings.general_shell_var.values():
+        parser.add_argument(*arg, **kwargs)
+
+    args: argparse.Namespace = parser.parse_args()
+    return vars(args)
 
 
 def main() -> None:
