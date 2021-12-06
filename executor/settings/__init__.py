0216e656c1715ea4a6c61b61ceb8e36abe681764
FlickerSoul <13360148+FlickerSoul@users.noreply.github.com>
2021-12-06 01:40:54 -0800

    fix: update settings module all

--- executor/settings/__init__.py
+++ executor/settings/__init__.py
@@ -1,1 +1,9 @@
-from .variables import VarClass, DefaultVars, SERVER_VERSION, IDENTIFIER_SEPARATOR
+from .variables import (
+    VarClass,
+    DefaultVars,
+    SERVER_VERSION,
+    IDENTIFIER_SEPARATOR,
+    RUNNER_ERROR_CODE,
+    CPU_OUT_EXIT_CODE,
+    MEM_OUT_EXIT_CODE,
+)
