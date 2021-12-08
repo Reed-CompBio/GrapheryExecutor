53bf794b32dad9169f03d5f51e94e9aaec94ebda
FlickerSoul <13360148+FlickerSoul@users.noreply.github.com>
2021-12-08 00:03:56 -0800

    fix: add logging to server result formatter

--- executor/server_utils/tools.py
+++ executor/server_utils/tools.py
@@ -19,9 +19,10 @@ class ServerError(Exception):
 
 
 class ServerResultFormatter:
-    def __init__(self) -> None:
+    def __init__(self, logger) -> None:
         self._errors = None
         self._info = None
+        self._logger = logger
 
     def format_server_result(self) -> str:
         return json.dumps({"errors": self._errors, "info": self._info})
@@ -34,6 +35,8 @@ class ServerResultFormatter:
         if not self._error_msg_valid(error_msg):
             raise ValueError("error message malformed")
 
+        self._logger.info("added error: \n" f"{error_msg}")
+
         self._errors.append(error_msg)
         return error_msg
 
@@ -45,11 +48,15 @@ class ServerResultFormatter:
         if not self._info_msg_valid(info_msg):
             raise ValueError("value message malformed")
 
+        self._logger.info("added info: \n" f"{info_msg}")
+
         self._info.append(info_msg)
         return info_msg
 
     def _error_msg_valid(self, error_msg: Dict) -> bool:
+        # TODO err msg validator
         return True
 
     def _info_msg_valid(self, info_msg: Dict) -> bool:
+        # TODO info msg validator
         return True
