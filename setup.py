f73a9d488561ed1991950ffea8c11eb72548b6a8
FlickerSoul <13360148+FlickerSoul@users.noreply.github.com>
2021-12-09 12:32:39 -0800

    fix: prog name in setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,8 @@ with open("executor/settings/variables.py") as fid:
     for line in fid:
         if line.startswith("SERVER_VERSION"):
             version = line.strip().split()[-1][1:-1]
-            break
+        if line.startswith("PROG_NAME"):
+            prog_name = line.strip().split()[-1][1:-1]
 
 
 setuptools.setup(
@@ -27,7 +28,7 @@ setuptools.setup(
         "networkg @ git+https://github.com/Reed-CompBio/networkx.git@networkg-2.6#egg=networkg"
     ],
     entry_points={
-        "console_scripts": ["graphery_executor=executor:main"],
+        "console_scripts": [f"{prog_name}=executor:main"],
     },
     long_description=read_file("README.md"),
     long_description_content_type="text/markdown",
